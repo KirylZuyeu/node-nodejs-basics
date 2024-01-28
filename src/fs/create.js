ee41c59fff88f171f4e3a9bfa3e542eb086a224e
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2024-01-28 12:11:32 +0100

    feat: add solution for subtasks fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from 'url';
+import {dirname, join} from 'path'
+import fsPromises from "fs/promises";
+const { access, writeFile } = fsPromises;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathForFile = join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await access(pathForFile);
+        const err = new Error('FS operation failed');
+        console.error(err.message);
+    } catch (error) {
+        await writeFile(pathForFile, 'I am fresh and young')
+        console.log('File has been created');
+    }
 };
 
 await create();
