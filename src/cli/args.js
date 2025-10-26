0cb39f3387552cc6c931ae80bec45df6693e2b15
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 21:45:26 +0100

    feat: args.js functionality

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-  // Write your code here
+  let argArr = [];
+  let arg = 0;
+
+  process.argv.forEach((val, index) => {
+    if (index > 1) {
+      if (index % 2 === 0) {
+        arg = val.slice(2);
+      } else {
+        argArr.push(`${arg} is ${val}`);
+      }
+    }
+  });
+
+  console.log(argArr.join(', '));
 };
 
 parseArgs();
