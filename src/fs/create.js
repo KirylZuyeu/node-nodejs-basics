4c6661118a2a843d27ce921ce8cd3ac16dfa96fd
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 20:35:41 +0100

    feat: add create.js function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import * as fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-  // Write your code here
-};
+  fs.access(filePath, function (error) {
+    if (error) {
+      fs.writeFile(filePath, 'I am fresh and young', (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    } else throw new Error('FS operation failed');
+  });
+}
 
 await create();
