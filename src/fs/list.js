d1ef351396f1dd628981587175ef2daef680c174
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 21:22:29 +0100

    feat: list.js functionality

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import * as fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filesPath = path.join(__dirname, 'files');
+const errorMessage = 'FS operation failed';
+
 const list = async () => {
-  // Write your code here
+    fs.access(filesPath, async (error) => {
+    if (!error) {
+      const items = await fs.promises.readdir(filesPath, {withFileTypes: true});
+      for (let item of items) {
+        console.log(item.name);
+      }
+    } else {
+      throw new Error(errorMessage);
+    }
+  });
 };
 
 await list();
