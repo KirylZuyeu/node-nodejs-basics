d4dbb07f8c96940d305ff2d51d645a179c1b4d69
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 21:26:49 +0100

    feat: read.js functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import * as fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filesPath = path.join(__dirname, 'files');
+const fileToReadPath = path.join(filesPath, 'fileToRead.txt');
+const errorMessage = 'FS operation failed';
+
 const read = async () => {
-  // Write your code here
+    fs.access(fileToReadPath, async (error) => {
+    if (!error) {
+      const fileContent = await fs.promises.readFile(fileToReadPath, 'utf8');
+      console.log(fileContent);
+    } else {
+      throw new Error('FS operation failed');
+    }
+  });
 };
 
 await read();
