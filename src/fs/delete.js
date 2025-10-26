0696d34af5168fee01414e4bcc1113169d4735d0
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 21:17:52 +0100

    feat: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import * as fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { rm } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filesPath = path.join(__dirname, 'files');
+const fileToRemove = path.join(filesPath, 'fileToRemove.txt');
+const errorMessage = 'FS operation failed';
+
 const remove = async () => {
-  // Write your code here
+    fs.access(fileToRemove, async function (error) {
+    if (!error) {
+      await rm(fileToRemove);
+    } else {
+      throw new Error(errorMessage);
+    }
+  });
 };
 
 await remove();
