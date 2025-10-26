6e5798c47d2dfd0a6225d01170bc15398fca5bda
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 22:13:42 +0100

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as crypto from 'crypto';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filesPath = path.join(__dirname, 'files');
+const fileToRead = path.join(filesPath, 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-  // Write your code here
+  const readStream = fs.createReadStream(fileToRead);
+  const hash = crypto.createHash('sha256');
+
+  return new Promise((resolve, reject) => {
+    readStream.pipe(hash);
+    readStream.on('end', () => {
+      const hexHash = hash.digest('hex');
+      console.log(hexHash);
+      resolve();
+    });
+  })
 };
 
 await calculateHash();
