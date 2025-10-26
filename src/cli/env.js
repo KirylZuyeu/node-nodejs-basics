845fde5f6ed4358da2f7e8140a484457af928df7
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 21:40:00 +0100

    feat: env.js functionality

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-  // Write your code here
+    let resultArr = [];
+    for (const prop in process.env) {
+        if (prop.startsWith('RSS_')) {
+            resultArr.push(`${prop}=${process.env[prop]}`);
+        }
+    }
+    console.log(resultArr.join('; '));
 };
-
 parseEnv();
