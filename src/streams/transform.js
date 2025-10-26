1572d6910425b2dad388b545fe7a28df56d6ff2e
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 22:38:23 +0100

    feat: streams transform.js functionslity

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-  // Write your code here
+  const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+      const transformResult = chunk.toString().split('').reverse().join('').trim();
+      callback(null, `${transformResult + '\n'}`);
+    },
+  });
+
+  process.stdin.pipe(reverse).pipe(process.stdout);
 };
 
 await transform();
