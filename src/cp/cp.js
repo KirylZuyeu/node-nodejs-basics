67e5a557d86edaebc7556f6b7628ae49118f90e7
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 23:17:30 +0100

    feat: cp functionality

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import * as childProcess from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filesPath = path.join(__dirname, 'files');
+const script = path.join(filesPath, 'script.js');
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const child = childProcess.fork(script, args);
+
+  child.on('message', (data) => {
+    process.stdout.write(`stdout: ${data}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const args = process.argv.slice(2);
+
+spawnChildProcess(args);
