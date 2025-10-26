fae4c37ca586e3375ec8dfc76cd91a359dfd7494
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-10-26 22:31:59 +0100

    feat: streams write.js functionality

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filesPath = path.join(__dirname, 'files');
+const fileToWrite = path.join(filesPath, 'fileToWrite.txt');
+
 const write = async () => {
-  // Write your code here
+  const writeStream = fs.createWriteStream(fileToWrite);
+  process.stdin.on('data', (data) => {
+    writeStream.write(data);
+  });
 };
 
 await write();
